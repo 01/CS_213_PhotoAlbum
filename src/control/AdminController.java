4da7f5898496900abed09706950a223d561aa116
Andrew Khazanovich <akhazanovich@gmail.com>
2017-04-10 17:26:26 -0400

    added currentAlbum static object to backEnd:

--- src/control/AdminController.java
+++ src/control/AdminController.java
@@ -1,38 +1,52 @@
 package control;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.ResourceBundle;
 
 import model.User;
 import model.Album;
 import model.BackendSerial;
 import model.Tag;
 import view.*;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
 
-public class AdminController {
+public class AdminController implements Initializable{
 	@FXML private Button addUserButton;
 	@FXML private Button deleteUserButton;
 	@FXML private Button logoutButton;
 
-	@FXML private TableView<User> usersTableView;
-	@FXML private TableColumn<User,String> userNameColumn;
-    @FXML private TableColumn<User,String> tagValueColumn;
-
-	
-	
-	/*myIntegerColumn.setCellValueFactory(cellData -> 
-	cellData.getValue().myIntegerProperty().asObject());*/
-	
+	@FXML
+	TableColumn<ObservableList<User>, String> username;
+	@FXML
+	TableColumn<ObservableList<User>, String> password;
+	@FXML
+	TableColumn<ObservableList<User>, String> firstName;
+	@FXML
+	TableColumn<ObservableList<User>, Calendar> dateCreated;
+	@FXML
+	TableColumn<ObservableList<User>, Calendar> lastLogin;
+	//More of your code 
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+	}
 	@FXML
 	public void onClick(ActionEvent e) throws IOException{	
 		Button clicked = (Button)e.getSource();
@@ -52,11 +66,20 @@ public class AdminController {
 		}
 	}
 	
-	public void initialize() {
-		ArrayList<User> users = BackendSerial.getUsers();
-		if(users.size() < 1) return;
+	/* public ObservableList<User> getUsersData() {
+	        return usersData;
+	    }
+	
+	 public void setUsersData(ObservableList<User> usersData) {
+	        
+	    }
+	public void setupUsersTable(){
+		ArrayList<User> userData = BackendSerial.getUsers();
+		ObservableList<User> usersData = FXCollections.observableList(userData);
 		
-		
-	}
+
+
+	}*/
+
 
 }
